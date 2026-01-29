86a47d6a9ab8535e9cf7333cd16442f8feb28f09
cristian_elmadrono <cristian.quiroga@avicolaelmadrono.com>
2026-01-29 17:00:46 -0500

    fix: update base path in Vite config for proper folder functionality

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
+      base: '/control-gastos/', // necesario para que funcione como carpetas
       server: {
         port: 3000,
         host: '0.0.0.0',
